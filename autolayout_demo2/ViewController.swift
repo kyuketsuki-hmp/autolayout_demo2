dfecbe302d78a69158125a84f4dc3e8885210b5e
HeinMyatPhone <hmyatphone@gmail.com>
2020-10-13 10:41:27 +0630

    Initial Commit

---
+++ autolayout_demo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autolayout_demo2
+//
+//  Created by kyuketsuki on 13/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
